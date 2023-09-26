e47a702b6ebb89ac4c7a869456f9f02ddf87e262
OlebogengKhunou <125869902+OlebogengKhunou@users.noreply.github.com>
2023-09-26 16:39:41 +0200

    Added navigation and Profile image

---
+++ FSJ7/pages/index.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import Hero from "../components/Home-page/hero"
+
+function HomePage(){
+    return (
+    <Fragment>
+        <Hero />
+    </Fragment>
+)}
+
+export default HomePage;
